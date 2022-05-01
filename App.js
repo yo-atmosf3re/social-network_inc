e11ab575f8535848682e6169cbc54eae079783b3
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-05-01 22:06:22 +0300

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <header>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png' />
+
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div>
+        Main content
+      </div>
+    </div >
+  );
+}
+
+
+export default App;
