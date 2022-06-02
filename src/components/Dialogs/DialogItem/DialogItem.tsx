0f8fae8df9e7155d696beaf3d32958746119d1e8
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-06-02 19:40:58 +0300

    update css

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -7,7 +7,8 @@ import s from './../Dialogs.module.css'
 export const DialogItem = (props: DialogItemType) => {
    return (
       <div className={s.dialog + ' ' + s.active}>
-         <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+         <NavLink to={'/dialogs/' + props.id}>{<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIZYSxc3XHXqhhaUedCBWKj9VX8s1K0_4hhrL_xaSJ06iaKEvyUWdQIK6BaIaTZjIxm-c&usqp=CAU" alt="" />
+         }{props.name}</NavLink>
       </div>
    )
 }
