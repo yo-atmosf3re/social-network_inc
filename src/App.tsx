6d27538272b6c5e8465583486e681b61849a2a84
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-06-04 23:12:06 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ const App = (props: RootStatePropsType) => {
         <Routes>
           <Route path='/profile' element={<Profile state={props.state.profilePage} />} />
           <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
+          <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage} />} />
         </Routes>
       </div>
     </div>
