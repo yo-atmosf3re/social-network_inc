b5f7f9a150ba13444a2da89aa10edbc05daf81c9
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-19 13:12:09 +0300

    creating an input form with logic, a password, a checkbox with "remember me" and a login button in the Login component; connecting the redux-forms library, adding typing for it;

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,7 +3,25 @@ import React from 'react'
 function Login() {
    return (<div>
       <h1>LOGIN</h1>
+      <LoginForm />
    </div>);
+}
+function LoginForm() {
+   return (
+      <form>
+         <div>
+            <input placeholder={'Login'} />
+         </div>
+         <div>
+            <input placeholder={'Password'} />
+         </div>
+         <div>
+            <input type={'checkbox'} /> remember me
+         </div>
+         <div>
+            <button>Login</button>
+         </div>
+      </form>);
 }
 
 export default Login;
